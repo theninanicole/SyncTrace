623dbfcc2a67c8fe8feef99149a410b28b21e7a6
theninanicole <villadareznn@gmail.com>
2026-09-19 22:03:23 +0800

    revert to orig google docs service

--- backend/src/main/java/com/ieee/evaluator/service/SubmissionSyncService.java
+++ backend/src/main/java/com/ieee/evaluator/service/SubmissionSyncService.java
@@ -1,5 +1,6 @@
 package com.ieee.evaluator.service;
 
+import com.google.api.services.drive.Drive;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import com.ieee.evaluator.model.DriveFile;
@@ -23,15 +24,19 @@ public class SubmissionSyncService {
     private final Sheets sheetsService;
     private final GoogleSheetsService configLoader;
     private final DynamicConfigService configService;
+    private final Drive driveService;
+
     private static final DateTimeFormatter TIMESTAMP_FORMATTER = DateTimeFormatter.ofPattern("M/d/yyyy H:mm:ss");
 
     public SubmissionSyncService(
             Sheets sheetsService,
             GoogleSheetsService configLoader,
-            DynamicConfigService configService) {
+            DynamicConfigService configService,
+            Drive driveService) {
         this.sheetsService = sheetsService;
         this.configLoader  = configLoader;
         this.configService = configService;
+        this.driveService  = driveService;
     }
 
     public List<DriveFile> getLatestSubmissions() throws IOException {
@@ -150,6 +155,18 @@ public class SubmissionSyncService {
             }
         }
 
+        // Fetch the real mimeType from Drive instead of hardcoding it
+        String mimeType;
+        try {
+            com.google.api.services.drive.model.File fileInfo = driveService
+                    .files().get(fileId).setFields("mimeType").execute();
+            mimeType = fileInfo.getMimeType() != null ? fileInfo.getMimeType() : "application/vnd.google-apps.document";
+        } catch (Exception e) {
+            // CRITICAL FIX: If the file is deleted or inaccessible, DO NOT add it to the map.
+            System.err.println("Skipping inaccessible fileId=" + fileId + ": " + e.getMessage());
+            return; // Exit the method so it isn't added to submissionMap
+        }
+
         DriveFile file = new DriveFile();
         file.setId(fileId);
         file.setWebViewLink(url);
@@ -157,7 +174,7 @@ public class SubmissionSyncService {
         String statusPrefix = isLate ? "[LATE] " : "";
         file.setName(statusPrefix + "[" + docType + "] " + section + " - " + teamCode + " | " + studentName);
         file.setSubmittedAt(timestampStr);
-        file.setMimeType("application/vnd.google-apps.document");
+        file.setMimeType(mimeType);
 
         submissionMap.put(fileId + "_" + docType, file);
     }
